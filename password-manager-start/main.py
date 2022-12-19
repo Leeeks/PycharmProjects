d6887cbc994e81517533fc4c866c2aa2becafcc5
Leeeks <aflekler@gmail.com>
2022-12-19 23:05:46 +0100

    Implemented password save functionality and messageboxes

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,12 +1,31 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     pass
 
-def add_password():
-    pass
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website_name = website_entry.get()
+    email_name = email_entry.get()
+    password = password_entry.get()
+
+    if len(password) == 0 or len(website_name) == 0:
+        messagebox.showerror("Something is wrong ...", "Either the website or your password is invalid.")
+    else:
+        is_ok = messagebox.askokcancel(title=website_name, message=f"These are the details entered:\nEmail: {email_name}"
+                                                           f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("myPasswords.txt", "a") as f:
+                entry_string = f"{website_name} | {email_name} | {password}"
+                f.write(f"{entry_string}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window
@@ -29,12 +48,14 @@ email_label.grid(row=2, column=0)
 password_label = Label(window, text="Password:", bg="white")
 password_label.grid(row=3, column=0)
 
-# Textbox
+# Entryboxes
 website_entry = Entry(width=43, bg="white")
 website_entry.grid(row=1, column=1, columnspan=2, sticky="w", padx=(10, 0))
+website_entry.focus()
 
 email_entry = Entry(width=43, bg="white")
 email_entry.grid(row=2, column=1, columnspan=2, sticky="w", padx=(10, 0))
+email_entry.insert(0, "aflekler@gmail.com")
 
 password_entry = Entry(width=24, bg="white")
 password_entry.grid(row=3, column=1, sticky="w", padx=(10, 0))
@@ -46,7 +67,7 @@ password_entry.grid(row=3, column=1, sticky="w", padx=(10, 0))
 generate_password_button = Button(text="Generate Password", command=generate_password, bg="white")
 generate_password_button.grid(row=3, column=1, columnspan=2, sticky="e")
 
-add_password_button = Button(width=36, text="Add", command=add_password, bg="white")
+add_password_button = Button(width=36, text="Add", command=save_password, bg="white")
 add_password_button.grid(row=4, column=1, columnspan=2, sticky="w", padx=(10, 0))
 
 window.mainloop()
