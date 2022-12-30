bff6344d0392714b84c4019f98a723682b106eff
Leeeks <aflekler@gmail.com>
2022-12-30 15:01:03 +0100

    updated error handling

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -24,15 +24,26 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 # TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#{"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_dictionary)
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Please input a word: ")
-print(list(user_word.upper()))
 
-phonetic_list = [nato_dictionary[letter] for letter in user_word.upper()]
+try_again = True
+while try_again:
+    try:
+        user_word = input("Please input a word: ")
+        print(list(user_word.upper()))
+        phonetic_list = [nato_dictionary[letter] for letter in user_word.upper()]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(phonetic_list)
+        try_again = False
 
-print(phonetic_list)
+
+
+
+
