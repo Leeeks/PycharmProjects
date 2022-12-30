c1f20d91902c0f9388a70c67fe27d754ec5f2955
Leeeks <aflekler@gmail.com>
2022-12-30 12:02:08 +0100

    Implemented generate password function

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,10 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for x in range(nr_letters)]
+    password_list += [random.choice(symbols) for x in range(nr_symbols)]
+    password_list += [random.choice(numbers) for x in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    # print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
@@ -15,16 +39,15 @@ def save_password():
     if len(password) == 0 or len(website_name) == 0:
         messagebox.showerror("Something is wrong ...", "Either the website or your password is invalid.")
     else:
-        is_ok = messagebox.askokcancel(title=website_name, message=f"These are the details entered:\nEmail: {email_name}"
-                                                           f"\nPassword: {password} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(title=website_name,
+                                       message=f"These are the details entered:\nEmail: {email_name}"
+                                               f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
             with open("myPasswords.txt", "a") as f:
                 entry_string = f"{website_name} | {email_name} | {password}"
                 f.write(f"{entry_string}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
